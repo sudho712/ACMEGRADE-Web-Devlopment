c6fca8d4ee1399f3632286977436fc37653ac014
Sudhir Kumar <sudho712@gmail.com>
2024-10-20 19:26:11 +0530

    connect

---
+++ inter.php
@@ -0,0 +1,11 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="ecom";
+$port=3306;
+
+$conn=new mysqli($host,$user,$pass,$db,$port)
+print_r($conn);
+echo "<br> Successfully connected";
+?>
